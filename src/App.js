42e95d90e3e728bb78e8537930c2bffd566ac210
Brett Spradling <brett@spradling.me>
2021-09-17 17:59:58 -0700

    wip

--- src/App.js
+++ src/App.js
@@ -1,45 +1,65 @@
 import React, { useState } from 'react';
-import clamp from 'lodash-es/clamp';
-import { useSpring, animated } from 'react-spring';
-import { useGesture } from 'react-with-gesture';
 
-import './App.css';
+import './styles/App.scss';
 
-function App() {
-  const [clicked, setClick] = useState(false);
-  const [{ xy }, set] = useSpring(() => ({ xy: [0, 0] }));
-  const bind = useGesture(({ down, delta, velocity }) => {
-    velocity = clamp(velocity, 1, 8);
-    set({
-      xy: down ? delta : [0, 0],
-      config: { mass: velocity, tension: 500 * velocity, friction: 50 }
+import Aside from './Aside.js'
+import { ProSidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
+import 'react-pro-sidebar/dist/css/styles.css';
+
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+
+export class MapContainer extends React.Component {
+  state = {
+    showingInfoWindow: false,
+    activeMarker: {},
+    selectedPlace: {},
+  };
+
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
     });
-  });
 
-  const _onClick = _ => setClick(!clicked);
+  onMapClicked = (props) => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      })
+    }
+  };
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div className="overlay" />
-        <div className="sticker">
-          <animated.div
-            {...bind()}
-            style={{
-              transform: xy.interpolate(
-                (x, y) => `translate3d(${x}px,${y}px,0)`
-              )
-            }}>
-            <span onClick={_onClick}>
-              {!clicked ? 'React-Tauri' : 'Hellö!'}
-            </span>
-          </animated.div>
-        </div>
-        <div className="container">
-        </div>
-      </header>
-    </div>
-  );
+  render() {
+    return (
+      <div>
+         <Aside
+             collapsed={false}
+             toggled={false}
+         />
+         <main>
+         <div>
+          <Map google={this.props.google}
+              onClick={this.onMapClicked}>
+            <Marker onClick={this.onMarkerClick}
+                    name={'Current location'} />
+
+            <InfoWindow
+              marker={this.state.activeMarker}
+              visible={this.state.showingInfoWindow}>
+                <div>
+                  <h1>{this.state.selectedPlace.name}</h1>
+                </div>
+            </InfoWindow>
+          </Map>
+          </div>
+          </main>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default GoogleApiWrapper({
+  apiKey: ('AIzaSyDV4L_eM1ETmSrOsMmFksP8bjdp_s0wIdg')
+})(MapContainer)
