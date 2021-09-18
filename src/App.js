4718e8db0943700469a456af5b5fae06031a8d75
Brett Spradling <brett@spradling.me>
2021-09-18 09:48:55 -0700

    sidebar and markers

--- src/App.js
+++ src/App.js
@@ -1,65 +1,186 @@
-import React, { useState } from 'react';
-
-import './styles/App.scss';
-
-import Aside from './Aside.js'
-import { ProSidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
-import 'react-pro-sidebar/dist/css/styles.css';
-
+import * as React from 'react';
+import { styled, useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import CssBaseline from '@mui/material/CssBaseline';
+import MuiAppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import notion from './Notion'
 
-export class MapContainer extends React.Component {
-  state = {
+const drawerWidth = 240;
+
+const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: `-${drawerWidth}px`,
+    ...(open && {
+      transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginLeft: 0,
+    }),
+  }),
+);
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+  transition: theme.transitions.create(['margin', 'width'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: `${drawerWidth}px`,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: 'flex-end',
+}));
+
+function Layout({google, locations = []}) {
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+  const [markerClick, setMarkerClick] = React.useState(true);
+  const [mapClick, setMapClick] = React.useState(true);
+  const [state, setState] = React.useState({
     showingInfoWindow: false,
     activeMarker: {},
     selectedPlace: {},
+  });
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
   };
 
-  onMarkerClick = (props, marker, e) =>
-    this.setState({
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+
+  const handleMarkerClick = (props, marker, e) =>
+    setState({
       selectedPlace: props,
       activeMarker: marker,
       showingInfoWindow: true
     });
 
-  onMapClicked = (props) => {
-    if (this.state.showingInfoWindow) {
-      this.setState({
+  const onMapClicked = (props) => {
+    if (state.showingInfoWindow) {
+      setState({
         showingInfoWindow: false,
         activeMarker: null
       })
     }
   };
 
-  render() {
-    return (
-      <div>
-         <Aside
-             collapsed={false}
-             toggled={false}
-         />
-         <main>
-         <div>
-          <Map google={this.props.google}
-              onClick={this.onMapClicked}>
-            <Marker onClick={this.onMarkerClick}
-                    name={'Current location'} />
-
-            <InfoWindow
-              marker={this.state.activeMarker}
-              visible={this.state.showingInfoWindow}>
-                <div>
-                  <h1>{this.state.selectedPlace.name}</h1>
-                </div>
-            </InfoWindow>
-          </Map>
-          </div>
-          </main>
-      </div>
-    )
-  }
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar position="fixed" open={open}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{ mr: 2, ...(open && { display: 'none' }) }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div">
+            Restaurants
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+          },
+        }}
+        variant="persistent"
+        anchor="left"
+        open={open}
+      >
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
+          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {['All mail', 'Trash', 'Spam'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <Main open={open}>
+        <DrawerHeader />
+        <Map google={google} containerStyle={{position: 'static', width: '100%', height: '100%'}} style={{width: '100%', height: '100%'}} onClick={onMapClicked}>
+          {notion.map(location => <Marker position={location.location}/>)}
+          <Marker onClick={handleMarkerClick} name={'Current location'} />
+          <InfoWindow
+            marker={state.activeMarker}
+            visible={state.showingInfoWindow}>
+              <div>
+                <h1>Brett Spradling</h1>
+              </div>
+          </InfoWindow>
+        </Map>
+      </Main>
+    </Box>
+  );
 }
 
 export default GoogleApiWrapper({
-  apiKey: ('AIzaSyDV4L_eM1ETmSrOsMmFksP8bjdp_s0wIdg')
-})(MapContainer)
+  apiKey: ('')
+})(Layout)
---
+++ src/Notion.js
@@ -0,0 +1,8 @@
+const locations = [
+  {
+    name: 'Din Tai Fung',
+    location: { lat: 47.6172005, lng: -122.2011831 }
+  }
+]
+
+export default locations;
